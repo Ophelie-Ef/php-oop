279ed29cb77698a2a3cee70e07d54c8f9a5c9b24
Ophelie <freville.ophelie@neuf.fr>
2023-12-04 12:12:23 +0100

    Affichage, notion sur private attribut

--- MaClass.php
+++ MaClass.php
@@ -6,7 +6,10 @@ class MaClass
     public $couleur = 'vert';
     public $age = '37';
     private $_attributPrivate; //par convention on commence un attribut privé par un underscore _
+    private $_nom = 'Michel PLIK';
 }
 
 $obj = new MaClass(); //creation d'un objet
-echo $obj->couleur;
+echo 'Couleur : '.$obj->couleur.'<br>';
+echo 'Age :'.$obj->age.'<br>';
+//echo 'Nom :'.$obj->_nom;      // Ne peux fonctionner : on ne peux acceder à un attribut privé en dehors de sa classe
