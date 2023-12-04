2625a0f8f77debb863c773de897d24948b2629f6
Ophelie <freville.ophelie@neuf.fr>
2023-12-04 12:19:25 +0100

    Creation d'une méthode

--- MaClass.php
+++ MaClass.php
@@ -7,9 +7,19 @@ class MaClass
     public $age = '37';
     private $_attributPrivate; //par convention on commence un attribut privé par un underscore _
     private $_nom = 'Michel PLIK';
+
+    public function displayMethode() // méthode
+    {
+        echo 'Je suis une méthode.'.'<br>';
+    }
+
 }
 
 $obj = new MaClass(); //creation d'un objet
+
 echo 'Couleur : '.$obj->couleur.'<br>';
 echo 'Age :'.$obj->age.'<br>';
+
 //echo 'Nom :'.$obj->_nom;      // Ne peux fonctionner : on ne peux acceder à un attribut privé en dehors de sa classe
+
+$obj->displayMethode();
